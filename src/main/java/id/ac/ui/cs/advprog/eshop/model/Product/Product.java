59d540e8ff9747ac0d368b34ba2971e44dcda97f
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-02-02 09:57:02 +0700

    add product model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product/Product.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.model.Product;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
+public class Product {
+    private String productId;
+    private String productName;
+    private int productQuantity;
+}
