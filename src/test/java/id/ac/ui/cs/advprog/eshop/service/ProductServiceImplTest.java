fbb7252d132152911fe81e40586086a45b269399
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-02-14 15:05:39 +0700

    FIX sonarcloud issue

--- src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImplTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
-public class ProductServiceImplTest {
+class ProductServiceImplTest {
     @InjectMocks
     ProductServiceImpl productService;
 
