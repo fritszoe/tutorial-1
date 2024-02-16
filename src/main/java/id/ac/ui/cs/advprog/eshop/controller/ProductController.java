cb5fd4d7fe67f743b47670931a647fa352e32cfa
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2024-02-16 15:33:59 +0700

    added CarController edit & delete

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -73,4 +73,38 @@ class CarController extends ProductController{
         return "createCar";
     }
 
+    @PostMapping("/createCar")
+    public String createCarPost(@ModelAttribute Car car, Model model) {
+        carService.create(car);
+        return "redirect:carList";
+    }
+
+    @GetMapping("/carList")
+    public String carListPage(Model model) {
+        Car car = new Car();
+        model.addAttribute("car", car);
+        return "carList";
+    }
+
+    @GetMapping("/editCar/{carId}")
+    public String editCarPage(@PathVariable String carId, Model model) {
+        Car car = carService.findById(carId);
+        model.addAttribute("car", car);
+        return "editCar";
+    }
+
+    @PostMapping("/editCar")
+    public String editCar(@ModelAttribute Car car, Model model){
+        System.out.println(car.getCarId());
+        carService.update(car.getCarId(), car);
+
+        return "redirect:carList";
+    }
+
+    @PostMapping("/deleteCar")
+    public String deleteCar(@RequestParam("carId") String carId) {
+        carService.delete(carId);
+        return "redirect:carList";
+    }
+
 }
